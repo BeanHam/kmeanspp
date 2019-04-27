7be1987159225e6d61aa3856b9b148d388cd5c63
Bin Han <39442036+BeanHam@users.noreply.github.com>
2019-04-27 12:49:23 -0400

    Update and rename kmeanspp_setup.py to setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='kmeanspp',
+      version='0.1',
+      description='Kmeans++ Clustering Algorithm',
+      url='https://github.com/BeanHam/kmeanspp',
+      author='Bin Han, Lingxi Song',
+      author_email='binhan96816@gmail.com',
+      license='MIT',
+      packages=['kmeanspp'],
+      zip_safe=False)
+
