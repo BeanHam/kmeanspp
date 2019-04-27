51b1bf223152e08ac64ef16a636f5f59ab4bcee3
Bin Han <39442036+BeanHam@users.noreply.github.com>
2019-04-27 12:48:07 -0400

    Add files via upload

---
+++ kmeanspp_setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import setup
+
+setup(name='kmeans',
+      version='0.1',
+      description='Kmeans Clustering Algorithm',
+      url='https://github.com/BeanHam/kmeans',
+      author='Bin Han, Lingxi Song',
+      author_email='binhan96816@gmail.com',
+      license='MIT',
+      packages=['kmeans'],
+      zip_safe=False)
+
